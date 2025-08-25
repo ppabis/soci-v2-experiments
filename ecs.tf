ce92cebc1c23d7cc7875f2b5b5b943ba6bb9f3c4
ppabis <piotr@pabis.eu>
2025-08-25 06:28:48 +0200

    Dummy ECS service

---
+++ ecs.tf
@@ -0,0 +1,28 @@
+resource "aws_ecs_cluster" "soci_cluster" {
+  name = "soci-cluster"
+}
+
+resource "aws_ecs_task_definition" "soci_task" {
+  family                   = "soci-task"
+  memory                   = 512
+  cpu                      = 256
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+  tags                     = { Name = "soci-task" }
+
+  container_definitions = jsonencode([
+    {
+      name      = "soci-container"
+      image     = "${aws_ecr_repository.soci_repo.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+    }
+  ])
+}

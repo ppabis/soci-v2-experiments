63487e94b33d78906726b7355323d38a4ca1c39f
ppabis <piotr@pabis.eu>
2025-08-27 07:01:02 +0200

    Create role for EC2 instance and add logs policy to ECS task role

--- iam.tf
+++ iam.tf
@@ -23,3 +23,50 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
 resource "aws_iam_role" "ecs_task_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
+
+resource "aws_iam_policy" "ecs_logs_write_policy" {
+  name        = "EcsSociLogsWritePolicy"
+  description = "Allow ECS tasks to create log groups/streams and put log events to all CloudWatch Logs resources"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_logs_write_policy_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_logs_write_policy.arn
+}
+
+data "aws_iam_policy_document" "ec2_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  assume_role_policy = data.aws_iam_policy_document.ec2_role.json
+}
+
+resource "aws_iam_instance_profile" "ec2_role" {
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_role_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}

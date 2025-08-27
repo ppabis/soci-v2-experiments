1f4927d075c3b87ef65936018fedbdecd105daca
ppabis <piotr@pabis.eu>
2025-08-27 07:01:49 +0200

    Output repo and registry

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,11 @@
 resource "aws_ecr_repository" "soci_repo" {
   name = "soci-repo"
 }
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.soci_repo.repository_url
+}
+
+output "ecr_registry_url" {
+  value = split("/", aws_ecr_repository.soci_repo.repository_url)[0]
+}
